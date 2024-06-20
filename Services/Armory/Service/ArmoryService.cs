23212f7c7326b02adcb41b3ac281f87b2f26eb35
Wootty2000 [2344687] <wootty2000@tornbot.com>
2024-06-20 22:19:13 +0100

    API LoadOut Fixes to LoadOut

--- Services/Armory/Service/ArmoryService.cs
+++ Services/Armory/Service/ArmoryService.cs
@@ -71,13 +71,13 @@ public class ArmoryService
             // Bonus with id and value as null, its been manually created to allow for String.Format to add the value correctly
             // We can fall back on id and value if the above hasnt been created
             if(
-                !_database.ArmoryItemRWBonus.Any(rwb => rwb.Id == dbArmoryItems.BonusId1 && rwb.Value == null) && 
-                !_database.ArmoryItemRWBonus.Any(rwb => rwb.Id == dbArmoryItems.BonusId1 && rwb.Value == dbArmoryItems.BonusId1)
+                !_database.ArmoryItemRWBonus.Any(rwb => rwb.BonusId == dbArmoryItems.BonusId1 && rwb.Value == null) && 
+                !_database.ArmoryItemRWBonus.Any(rwb => rwb.BonusId == dbArmoryItems.BonusId1 && rwb.Value == dbArmoryItems.BonusId1)
             )
             {
                 bonus = new ArmoryItemRWBonus
                 {
-                    Id = armoryItem.Bonuses[0].Id,
+                    BonusId = armoryItem.Bonuses[0].Id,
                     Value = armoryItem.Bonuses[0].Value,
                     Bonus = armoryItem.Bonuses[0].Title,
                     Description = armoryItem.Bonuses[0].Description
@@ -94,13 +94,13 @@ public class ArmoryService
             dbArmoryItems.BonusVal2 = armoryItem.Bonuses[1].Value;
 
             if (
-                !_database.ArmoryItemRWBonus.Any(rwb => rwb.Id == dbArmoryItems.BonusId2 && rwb.Value == null) &&
-                !_database.ArmoryItemRWBonus.Any(rwb => rwb.Id == dbArmoryItems.BonusId2 && rwb.Value == dbArmoryItems.BonusId2)
+                !_database.ArmoryItemRWBonus.Any(rwb => rwb.BonusId == dbArmoryItems.BonusId2 && rwb.Value == null) &&
+                !_database.ArmoryItemRWBonus.Any(rwb => rwb.BonusId == dbArmoryItems.BonusId2 && rwb.Value == dbArmoryItems.BonusId2)
             )
             {
                 bonus = new ArmoryItemRWBonus
                 {
-                    Id = armoryItem.Bonuses[1].Id,
+                    BonusId = armoryItem.Bonuses[1].Id,
                     Value = armoryItem.Bonuses[1].Value,
                     Bonus = armoryItem.Bonuses[1].Title,
                     Description = armoryItem.Bonuses[1].Description
@@ -117,13 +117,13 @@ public class ArmoryService
             dbArmoryItems.BonusVal3 = armoryItem.Bonuses[2].Value;
 
             if (
-                !_database.ArmoryItemRWBonus.Any(rwb => rwb.Id == dbArmoryItems.BonusId3 && rwb.Value == null) &&
-                !_database.ArmoryItemRWBonus.Any(rwb => rwb.Id == dbArmoryItems.BonusId3 && rwb.Value == dbArmoryItems.BonusId3)
+                !_database.ArmoryItemRWBonus.Any(rwb => rwb.BonusId == dbArmoryItems.BonusId3 && rwb.Value == null) &&
+                !_database.ArmoryItemRWBonus.Any(rwb => rwb.BonusId == dbArmoryItems.BonusId3 && rwb.Value == dbArmoryItems.BonusId3)
             )
             {
                 bonus = new ArmoryItemRWBonus
                 {
-                    Id = armoryItem.Bonuses[2].Id,
+                    BonusId = armoryItem.Bonuses[2].Id,
                     Value = armoryItem.Bonuses[2].Value,
                     Bonus = armoryItem.Bonuses[2].Title,
                     Description = armoryItem.Bonuses[2].Description
@@ -197,13 +197,13 @@ public class ArmoryService
     {
 
         Database.Entities.ArmoryItemRWBonus? dbBonus = 
-            _database.ArmoryItemRWBonus.Where(b => b.Id == id && b.Value == null).FirstOrDefault();
+            _database.ArmoryItemRWBonus.Where(b => b.BonusId == id && b.Value == null).FirstOrDefault();
         TornBot.Entities.ItemRankedWarBonus bonus = new ItemRankedWarBonus();
 
         if (dbBonus == null)
         {
             dbBonus = _database.ArmoryItemRWBonus.FirstOrDefault(b =>
-                b.Id == id && b.Value == value);
+                b.BonusId == id && b.Value == value);
             bonus.Description = dbBonus.Description;
         }
         else
